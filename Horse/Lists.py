a6c9acb1974b289a7138ced6fac2fc76e171c174
bcwan <bcwan8@gmail.com>
2020-07-09 22:58:09 -0700

    Add list methods

--- Horse/Lists.py
+++ Horse/Lists.py
@@ -4,5 +4,14 @@ other_lucky_numbers = [4, 5, 6]
 # mutates lucky_numbers
 lucky_numbers.extend(other_lucky_numbers)
 
-print(lucky_numbers)
+# adding new items / like .push in JS
+lucky_numbers.append(7)
 
+lucky_numbers.insert(0, 0)
+
+print(lucky_numbers)
+# [0, 1, 2, 3, 4, 5, 6, 7]
+
+lucky_numbers.remove(0)
+
+print(lucky_numbers)
