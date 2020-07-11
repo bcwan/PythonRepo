fb055950c3d46d2af444abbc3796a9594beb75f1
bcwan <bcwan8@gmail.com>
2020-07-11 00:13:35 -0700

    Add Python dictionary example

---
+++ Horse/Dictionaries.py
@@ -0,0 +1,18 @@
+monthConversions = {
+  "Jan": "January",
+  "Feb": "February",
+  "Mar": "March",
+  "Apr": "April",
+  "May": "May",
+  "Jun": "June",
+  "Jul": "July",
+  "Aug": "August",
+  "Sep": "September",
+  "Oct": "October",
+  "Nov": "November",
+  "Dec": "December"
+}
+
+print(monthConversions["Nov"])
+print(monthConversions.get("Dec"))
+print(monthConversions.get("Luv", "Not a default value"))
