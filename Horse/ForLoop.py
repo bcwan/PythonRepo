06ca3fa33ffe961ce992f54ee0e4498e8eed62bd
bcwan <bcwan8@gmail.com>
2020-07-11 23:52:02 -0700

    Add for loops in Python

---
+++ Horse/ForLoop.py
@@ -0,0 +1,19 @@
+for letter in "Horse Barn":
+  print(letter)
+
+friends = ["Horse", "Skipper", "Butter"]
+
+for friend in friends:
+  print(friend)
+
+for index in range(3, 10):
+  print(index)
+
+for index in range(len(friends)):
+  print(friends[index])
+
+for index in range(5):
+  if index == 0:
+    print("First iteration")
+  else:
+    print("Not first")
