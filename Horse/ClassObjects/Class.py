e8f9501aafcccc0e899651cd4f086b611399f0a6
bcwan <bcwan8@gmail.com>
2020-07-12 21:41:02 -0700

    Create class functions

--- Horse/ClassObjects/Class.py
+++ Horse/ClassObjects/Class.py
@@ -1,5 +1,7 @@
 from Student import Student
 
-student1 = Student("Brian", "Computer Science", 3.3, False)
+student1 = Student("Brian", "Computer Science", 3.3)
+student2 = Student("Shaina", "Computer Science", 3.5)
 
-print(student1.name)
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
--- Horse/ClassObjects/Student.py
+++ Horse/ClassObjects/Student.py
@@ -1,7 +1,12 @@
 class Student:
   #similar to constructor in javascript
-  def __init__(self, name, major, gpa, is_on_probation):
+  def __init__(self, name, major, gpa):
     self.name = name
     self.major = major
     self.gpa = gpa
-    self.is_on_probation = is_on_probation
+
+  def on_honor_roll(self):
+    if self.gpa >= 3.5:
+      return True
+    else:
+      return False
