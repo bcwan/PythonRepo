d8b499c145a131b0a9717ebc9f1e438ba8abbe49
bcwan <bcwan8@gmail.com>
2020-07-12 21:48:17 -0700

    Create a Chef class for inheritance tutorial

---
+++ Horse/Inheritance/Chef.py
@@ -0,0 +1,10 @@
+class Chef:
+  
+  def make_chicken(self):
+    print("Cook the chicken!")
+
+  def make_salad(self):
+    print("Make the salad.")
+
+  def make_special_dish(self):
+    print("Make a special dish tonight!")
