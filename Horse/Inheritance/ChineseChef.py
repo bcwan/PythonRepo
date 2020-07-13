0e064922c3b325dea671e6948f6a3fe8637acd4d
bcwan <bcwan8@gmail.com>
2020-07-12 22:01:14 -0700

    Override parent method in class

--- Horse/Inheritance/ChineseChef.py
+++ Horse/Inheritance/ChineseChef.py
@@ -2,5 +2,9 @@ from Chef import Chef
 
 class ChineseChef(Chef):
 
+  #override
+  def make_special_dish(self):
+    print("The chef makes orange dish")
+
   def make_fried_rice(self):
     print("The chef makes fried rice")
--- Horse/Inheritance/People.py
+++ Horse/Inheritance/People.py
@@ -2,8 +2,8 @@ from Chef import Chef
 from ChineseChef import ChineseChef
 
 myChef = Chef()
-myChef.make_chicken()
+myChef.make_special_dish()
 
 
 myChef = ChineseChef()
-myChef.make_fried_rice()
+myChef.make_special_dish()
