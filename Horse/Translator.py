c52f8810d213cf83431f7781a40075a6818d59b6
bcwan <bcwan8@gmail.com>
2020-07-12 18:51:31 -0700

    Translate uppercase letters

--- Horse/Translator.py
+++ Horse/Translator.py
@@ -2,7 +2,10 @@ def translate(phrase):
   translation = ""
   for letter in phrase:
     if letter in "AEIOUaeiou":
-      translation = translation + "g"
+      if letter.isupper():
+        translation = translation + "G"
+      else: 
+        translation = translation + "g"
     else:
       translation = translation + letter
   return translation
